90315f14e02ffe8886d0505f49d8a07ddf41d5b8
stevana <stevana@users.noreply.github.com>
2024-06-25 11:40:43 +0000

    Deploying to gh-pages from @ stevana/stevana.github.io@083ab7895b1b486b72890cdfa9a33da6c62d1d55 🚀

---
+++ script.js
@@ -0,0 +1,27 @@
+// Taken from:
+// https://css-tricks.com/sticky-table-of-contents-with-scrolling-active-states/
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            const id = entry.target.getAttribute('id');
+            if (entry.intersectionRatio > 0) {
+                const elem = document.querySelector(`main > nav li a[href="#${id}"]`);
+                if (elem) {
+                        elem.parentElement.classList.add('active');
+                }
+            } else {
+                const elem = document.querySelector(`main > nav li a[href="#${id}"]`);
+                if (elem) {
+                    elem.parentElement.classList.remove('active');
+                }
+            }
+        });
+    });
+
+    document.querySelectorAll('section[id]').forEach((section) => {
+        observer.observe(section);
+    });
+
+});
